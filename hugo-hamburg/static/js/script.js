f24bd419a6c59307f3081bbc400501efa3f8950b
Markus Voge <mvoge@posteo.de>
2019-03-22 09:27:38 +0100

    Fix navbar styling

--- hugo-hamburg/static/js/script.js
+++ hugo-hamburg/static/js/script.js
@@ -2,11 +2,7 @@
 window.addEventListener("scroll", function() { // scroll touchmove
   if (window.scrollY > 0) {
     document.getElementById('navbar-outer').classList.add('navbar-tiny');
-    document.getElementById('navbar-header').classList.add('navbar-header-tiny');
   } else {
     document.getElementById('navbar-outer').classList.remove('navbar-tiny');
-    document.getElementById('navbar-header').classList.remove('navbar-header-tiny');
   }
-  // $('.navbar-nav').toggleClass('navbar-nav-tiny', $(document).scrollTop() > 0);
-  // $('.navbar-brand').toggleClass('navbar-brand-tiny', $(document).scrollTop() > 0);
 });
