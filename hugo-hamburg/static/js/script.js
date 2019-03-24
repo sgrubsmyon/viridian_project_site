5506eec2be06a205ff5300dd82b3d7aaccd4665a
Markus Voge <mvoge@posteo.de>
2019-03-24 20:57:17 +0100

    More non-JS friendly: navbar tiny on default

--- hugo-hamburg/static/js/script.js
+++ hugo-hamburg/static/js/script.js
@@ -1,8 +1,11 @@
 // https://stackoverflow.com/questions/16442016/jquery-sticky-header-that-shrinks-when-scrolling-down
-window.addEventListener("scroll", function() { // scroll touchmove
+function navbarTinyToggler() { // scroll touchmove
   if (window.scrollY > 0) {
     document.getElementById('navbar-outer').classList.add('navbar-tiny');
   } else {
     document.getElementById('navbar-outer').classList.remove('navbar-tiny');
   }
-});
+}
+
+window.addEventListener("scroll", navbarTinyToggler);
+window.onload = navbarTinyToggler;
